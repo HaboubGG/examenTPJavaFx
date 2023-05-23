8867f8a4c770416ee5137a714057b4e1e14dc30c
habib <dhaouadimedhbib@gmail.com>
2023-05-23 12:42:17 +0100

    dfqqsdfds

---
+++ src/main/java/com/example/examentpjavafx/SampleController.java
@@ -0,0 +1,27 @@
+package com.example.examentpjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SampleController {
+@FXML
+    TextField textfield;
+@FXML
+Label result;
+@FXML
+    public void save()
+{
+
+}
+@FXML
+    public void convert()
+{
+
+}
+@FXML
+    public void cancel()
+{
+
+}
+}
